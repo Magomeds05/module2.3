858bb3f3348757a1ab89a17b0479f2a2e7e22885
Magomeds05 <yusypovm@gmail.com>
2024-06-12 13:51:54 +0300

    Add files via upload

---
+++ module2.3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = [0]
+positive_numbers = []
+while i < len(my_list):
+    if my_list(i) < i:
+        break
+    if my_list(i) > i:
+        positive_numbers.append(my_list[i])
+
+print(positive_numbers)
